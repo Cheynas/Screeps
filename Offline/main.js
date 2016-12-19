5abcfea9f948722515895666b1efcbae17976d08
Alexis Cheynas <AlexisCheynas@gmail.com>
2016-12-19 08:43:44 +0100

    Added simple memory management on main script

--- Offline/main.js
+++ Offline/main.js
@@ -1,3 +1,8 @@
 module.exports.loop = function () {
-
+	for (var name in Memory.creeps) {
+		if (!Game.creeps[name]) {
+			console.log('Creep expired:', name);
+			delete(Memory.creeps[name]);
+		}
+	}
 }
