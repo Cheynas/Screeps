6826558b2762cf3394aa9e4bf7896822accc2ed4
Alexis Cheynas <AlexisCheynas@gmail.com>
2016-12-19 08:51:37 +0100

    Made the memory management keep track of the number of creeps, and in what role, there are. Also saying what kind of creep expired, when it does expire.

--- Offline/main.js
+++ Offline/main.js
@@ -3,11 +3,18 @@ creep.builder = require('creep.builder');
 creep.harvester = require('creep.harvester');
 creep.upgrader = require('creep.upgrader');
 
+if (!Memory.count) { Memory.count = {} };
+if (!Memory.count.builder) { Memory.count.builder = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder').length };
+if (!Memory.count.harvester) { Memory.count.harvester = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester').length };
+if (!Memory.count.upgrader) { Memory.count.upgrader = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader').length };
+
 module.exports.loop = function () {
 	for (var name in Memory.creeps) {
 		if (!Game.creeps[name]) {
-			console.log('Creep expired:', name);
+			var role = Memory.creeps[name].role;
+			console.log(role, 'expired:', name);
 			delete(Memory.creeps[name]);
+			Memory.count[role] -= 1;
 		}
 	}
 
