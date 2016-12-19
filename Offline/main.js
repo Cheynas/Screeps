3f8607c182aba034d6595504a9bd7a0d750618c6
Alexis Cheynas <AlexisCheynas@gmail.com>
2016-12-19 08:49:40 +0100

    Added creep roles

--- Offline/main.js
+++ Offline/main.js
@@ -1,8 +1,18 @@
+var creep = {};
+creep.builder = require('creep.builder');
+creep.harvester = require('creep.harvester');
+creep.upgrader = require('creep.upgrader');
+
 module.exports.loop = function () {
 	for (var name in Memory.creeps) {
 		if (!Game.creeps[name]) {
 			console.log('Creep expired:', name);
 			delete(Memory.creeps[name]);
 		}
+	}
+
+	for (var name in Game.creeps) {
+		var role = Memory.creeps[name].role;
+		creep[role].run(Game.creeps[name]);
 	}
 }
