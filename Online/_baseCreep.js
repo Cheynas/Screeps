79fb1f13cd3788056eacae856419238ed143aeb3
Alexis Cheynas <AlexisCheynas@gmail.com>
2017-04-06 23:29:52 +0200

    Copied the Offline files to Online

--- Online/_baseCreep.js
+++ Online/_baseCreep.js
@@ -7,8 +7,7 @@
  * mod.thing == 'a thing'; // true
  */
 
-function Creep() {}
-Creep.prototype.role = null;
+function Creep(role) { this.role = role; }
 
 Creep.prototype.tiers = [[WORK,CARRY,MOVE]]; /* Lowest viable creep */
 
@@ -39,11 +38,125 @@ Creep.prototype.nav = function(creep, target) {
 	if (target == null) return ERR_INVALID_TARGET;
 	if (creep.pos.isNearTo(target)) return OK;
 
-	return creep.moveTo(target);
+	return creep.moveTo(target, {visualizePathStyle: {}});
 }
 
 Creep.prototype.run = function(creep) {
 	creep.say('ERROR!');
 }
+
+Creep.prototype.gather = function(creep) {
+	var drop = creep.pos.findClosestByPath(FIND_DROPPED_ENERGY);
+	if (drop) {
+		if (creep.pos.isNearTo(drop)) return creep.pickup(drop);
+		else return this.nav(creep, drop);
+	}
+
+	if (creep.carry.energy > creep.carryCapacity/2) creep.memory.gather = false;
+	var container = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => {
+		return (
+			structure.structureType == STRUCTURE_CONTAINER ||
+			structure.structureType == STRUCTURE_LINK ||
+			structure.structureType == STRUCTURE_STORAGE
+			) && structure.store[RESOURCE_ENERGY] > 0;
+	}});
+	if (container) {
+		if (creep.pos.isNearTo(container)) return creep.withdraw(container, RESOURCE_ENERGY);
+		else return this.nav(creep, container);
+	}
+
+	var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+	if (source) {
+		if (creep.pos.isNearTo(source)) return creep.harvest(source);
+		else return this.nav(creep, source);
+	} else return ERR_NOT_FOUND;
+}
+
+Creep.prototype.build = function(creep) {
+	if (_.size(Game.constructionSites) == 0) return ERR_NOT_FOUND;
+
+	var target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+	if (target) {
+		if (creep.pos.inRangeTo(target, 3)) return creep.build(target);
+		else return this.nav(creep, target);
+	} else return ERR_NOT_FOUND;
+}
+
+Creep.prototype.repair = function(creep) {
+	var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => {
+		return structure.hits < 1000 && structure.hits < structure.hitsMax;
+	}});
+
+	if (!target) target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => {
+		return (
+			structure.structureType != STRUCTURE_WALL &&
+			structure.structureType != STRUCTURE_RAMPART &&
+			structure.structureType != STRUCTURE_ROAD
+			) && structure.hits < structure.hitsMax;
+	}});
+	if (!target) target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => {
+		return structure.structureType == STRUCTURE_RAMPART && structure.hits < structure.hitsMax;
+	}});
+	if (!target) target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => {
+		return structure.structureType == STRUCTURE_ROAD && structure.hits < structure.hitsMax;
+	}});
+	if (!target) target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => {
+		return structure.structureType == STRUCTURE_WALL && structure.hits < structure.hitsMax;
+	}});
+
+	if (target) {
+		if (creep.pos.inRangeTo(target, 3)) return creep.repair(target);
+		else return this.nav(creep,target);
+	} else return ERR_NOT_FOUND;
+}
+
+Creep.prototype.upgrade = function(creep) {
+	if (creep.room.controller.my) {
+		if (creep.pos.inRangeTo(creep.room.controller, 3)) return creep.upgradeController(creep.room.controller);
+		else return this.nav(creep, creep.room.controller);
+	} else return ERR_NOT_FOUND;
+};
+
+Creep.prototype.haul = function(creep) {
+	var target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {filter: (structure) => {
+		return structure.structureType == STRUCTURE_TOWER && structure.energy < structure.energyCapacity;
+	}})
+
+	if (!target) target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {filter: (structure) => {
+		return (
+			structure.structureType == STRUCTURE_EXTENSION ||
+			structure.structureType == STRUCTURE_TOWER ||
+			structure.structureType == STRUCTURE_SPAWN
+		) && structure.energy < structure.energyCapacity;
+	}});
+
+	if (target) {
+		if (creep.pos.isNearTo(target)) return creep.transfer(target, RESOURCE_ENERGY);
+		else return this.nav(creep,target);
+	} else return ERR_NOT_FOUND;
+}
+
+Creep.prototype.harvest = function(creep) {
+	if (_.sum(creep.carry)) {
+		var container = creep.pos.findInRange(FIND_STRUCTURES, 1, {filter: (structure) => {
+			return (
+				structure.structureType == STRUCTURE_CONTAINER ||
+				structure.structureType == STRUCTURE_LINK ||
+				structure.structureType == STRUCTURE_STORAGE
+				) && (_.sum(structure.store) < structure.storeCapacity);
+		}});
+		if (container.length) creep.transfer(container[0], RESOURCE_ENERGY);
+	}
+
+	if (_.sum(creep.carry) == creep.carryCapacity) return ERR_FULL;
+
+	var target = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+	if (!target) target creep.pos.findClosestByPath(FIND_SOURCES);
+
+	if (target) {
+		if (creep.pos.isNearTo(target)) return creep.harvest(target);
+		else return this.nav(creep, target);
+	} else return ERR_NOT_FOUND;
+};
 
 module.exports = Creep;
